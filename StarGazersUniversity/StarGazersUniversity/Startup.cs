765cdeef44145377e05026592b3983bf59e74e60
Luay <luayghazwan@yahoo.com>
2018-01-10 13:22:22 -0800

    Dependency injection for DbContext

--- StarGazersUniversity/StarGazersUniversity/Startup.cs
+++ StarGazersUniversity/StarGazersUniversity/Startup.cs
@@ -5,17 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StarGazersUniversity.Data;
 
 namespace StarGazersUniversity
 {
     public class Startup
     {
+        // Using Dependency injection to add configuration to the startup to use the Student Db Context
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            configuration = Configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Registering the context in the configuration service
+            services.AddDbContext<StudentDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
