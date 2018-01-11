e50e6f5726f154bd4cfe240d97c955345a5fed7b
Luay <luayghazwan@yahoo.com>
2018-01-10 21:01:56 -0800

    Controller methods with context injected

--- StarGazersUniversity/StarGazersUniversity/Controllers/EnrollmentController.cs
+++ StarGazersUniversity/StarGazersUniversity/Controllers/EnrollmentController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StarGazersUniversity.Data;
 using StarGazersUniversity.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,10 +13,23 @@ namespace StarGazersUniversity.Controllers
 {
     public class EnrollmentController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult ViewAllStudents(Student[] students)
+        private readonly StudentDbContext _context;
+        public EnrollmentController(StudentDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // Release the allocated resources for Student Context
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpGet]
+        public IActionResult ViewAllStudents()
+        {
+            List<Student> students = _context.Students.ToList();
+            return View(students);
         }
     }
 }
--- StarGazersUniversity/StarGazersUniversity/Controllers/HomeController.cs
+++ StarGazersUniversity/StarGazersUniversity/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StarGazersUniversity.Data;
 using StarGazersUniversity.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,10 +12,35 @@ namespace StarGazersUniversity.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        // Bringing the context to this controller.
+        private readonly StudentDbContext _context;
+        public HomeController(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+        // Release the allocated resources for Student Context
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Student student)
+        {
+            if (!String.IsNullOrEmpty(student.FirstName))
+            {
+                _context.Add(student);
+                _context.SaveChanges();
+            }
+            
+            return RedirectToAction("ViewAllStudents", "Enrollment");
         }
     }
 }
